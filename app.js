c3ef947f6fedab1f28da912b4664b81fa5f7b5f7
daizyu0807 <daizyu0807@gmail.com>
2022-02-01 10:53:30 +0800

    feat: add index.hbs and main.hbs in views

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const port = 3000
-
+const exphbs = require('express-handlebars')
 const app = express()
 
 require('./config/mongoose') //引用 mongoose
 
 app.get('/', (req, res) => {
-  res.send('express is running in FE')
+  res.render('index')
 })
+
+// 設定 handlebars
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'})) // 建立 hbs 模版並引入參數
+app.set('view engine', 'hbs') // 啟用樣板引擎 hbs
 
 app.listen(port, () => {
   console.log(`user-login is running on http://localhost:${port}`)
